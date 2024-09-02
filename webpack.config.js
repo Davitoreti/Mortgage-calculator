a23c17b54315a4b58a598689add5dc5fab157d2f
Davi <0341907@adm.feevale.br>
2024-09-02 18:21:24 -0300

    Finished project

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
-const htmlWebpackPlugin = require('html-webpack-plugin');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-const plugin = require('tailwindcss');
 
 module.exports = {
     entry: {
@@ -9,36 +7,40 @@ module.exports = {
     },
     mode: 'production',
     module: {
-        rules: [{
-            test: /\.js$/,
-            use: ['babel-loader']
-        },{
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader', 'postcss-loader']
-        },{
-            test: /\.(png|jpe?g|gif|svg)$/i,
-            use: [{
-                loader: 'file-loader',
-                options: {
-                    name: 'images/[name].[ext]',
-                    outputPath: 'images',
-                    publicPath: 'images',
+        rules: [
+            {
+                test: /\.js$/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset',
+                generator: {
+                    filename: 'images/[name][hash:7][ext]'
                 },
-                loader: 'url-loader',
-                options: {
-                    limit: 8192, // 8kb, arquivos menores que isso serão convertidos para base64
-                    name: '[name].[hash:7].[ext]', // nome do arquivo com hash para cache busting
-                    outputPath: 'images/', // diretório de saída para as imagens
-                },
-            }]
-        }]
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 15000 // 15kb
+                    }
+                }
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            }
+        ]
     },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
     },
     plugins: [
-        new htmlWebpackPlugin({
+        new HtmlWebpackPlugin({
             template: './src/index.html',
             filename: 'index.html' 
         })
